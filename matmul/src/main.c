1a93b4e48d84acfe233de26597ccdca7fd8727cd
Cristobal <cristobal.ramirez.lazo@gmail.com>
2024-08-13 00:52:27 -0500

    Add Matmul kernel
    inputs are pending to be added
    
    Not tested

---
+++ matmul/src/main.c
@@ -0,0 +1,134 @@
+/*************************************************************************
+** * * * * * * * * *  MATRIX MULTIPLICATION * * * * * * * * * * * * * * ** 
+**************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include "utils.h" 
+
+#include "../../common/riscv_util.h"
+
+#ifdef USE_RISCV_VECTOR
+//#include "../../common/vector_defines.h"
+#endif
+
+#define DATA_TYPE 
+typedef double data_t;
+
+#ifdef USE_RISCV_VECTOR
+static void matrixmul_intrinsics( size_t dm, size_t dk, size_t dn, data_t *c , data_t *a, data_t *b ) ;
+#else // !USE_RISCV_VECTOR
+static void matmul_serial( size_t dm, size_t dk, size_t dn, data_t *c , data_t *a, data_t *b ) ;
+#endif
+static bool compare( size_t dm, size_t dn, data_t *a , data_t *b) ;
+
+
+int main (int argc, char **argv)
+{
+    FILE *file;
+    data_t * buffer;
+    int rv;
+
+    if (argc != 4){
+        printf("Usage:\n\t%s <Rows> <Colums> <inputFile>\n", argv[0]);
+        xit(1);
+    }
+
+    int M = atoi(argv[1]);
+    int N = atoi(argv[1]);
+    int K = N;
+    char *inputFile = argv[3];
+
+    //Read input data from file
+    file = fopen(inputFile, "r");
+    if(file == NULL) {
+      printf("ERROR: Unable to open file `%s'.\n", inputFile);
+      exit(1);
+    }
+
+    rv = fscanf(file, "%i", &numOptions);
+    if(rv != 1) {
+      printf("ERROR: Unable to read from file `%s'.\n", inputFile);
+      fclose(file);
+      exit(1);
+    }
+
+    data_t *M1 = (data_t*)malloc(M*N*sizeof(data_t));
+    data_t *M2 = (data_t*)malloc(N*K*sizeof(data_t));
+    data_t *reference = (data_t*)malloc(M*N*sizeof(data_t));
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            rv = fscanf(file, "%f", M1[i*N+j]);
+            if(rv != M*N) {
+              printf("ERROR: Unable to read from file `%s'.\n", inputFile);
+              fclose(file);
+              exit(1);
+            }
+        }
+ 
+    }
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            rv = fscanf(file, "%f", M2[i*N+j]);
+            if(rv != M*N) {
+              printf("ERROR: Unable to read from file `%s'.\n", inputFile);
+              fclose(file);
+              exit(1);
+            }
+        }
+ 
+    }
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            rv = fscanf(file, "%f", reference[i*N+j]);
+            if(rv != M*N) {
+              printf("ERROR: Unable to read from file `%s'.\n", inputFile);
+              fclose(file);
+              exit(1);
+            }
+        }
+ 
+    }
+
+
+    rv = fclose(file);
+    if(rv != 0) {
+      printf("ERROR: Unable to close file `%s'.\n", inputFile);
+      exit(1);
+    }
+
+    
+    long long start,end;
+    start = get_time();
+
+
+#ifdef USE_RISCV_VECTOR
+
+    matrixmul_intrinsics(M, K, N, result, M1, M2);
+    printf("matrixmul_intrinsics done\n");
+    
+    end = get_time();
+    printf("matrixmul_intrinsics time: %f\n", elapsed_time(start, end));
+
+#else // !USE_RISCV_VECTOR
+
+    matmul_serial(M, K, N, result, M1, M2);
+    printf("matmul_serial done\n");
+    
+    end = get_time();
+    printf("matmul_serial time: %f\n", elapsed_time(start, end));
+#endif
+
+    
+    if(compare(M, N, result, reference)){
+        printf("Verification failed!\n");
+        return 1;
+    }
+
+    return 0;
+}
---
+++ matmul/src/matmul.c
@@ -0,0 +1,70 @@
+/*************************************************************************
+* Vectorized matrixmul Kernel
+*************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+#define DATA_TYPE 
+typedef double data_t;
+
+#ifdef USE_RISCV_VECTOR
+
+#include <riscv_vector.h>
+
+static void matrixmul_intrinsics(data_t *a, data_t *b, data_t *c, int n, int m, int p) {
+
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < m; j++) {
+            size_t gvl = __riscv_vsetvl_e64m1(p);
+            vfloat64m1_t vprod = __riscv_vfmv_v_f_f64m1(0, gvl); 
+            vfloat64m1_t vsum  = __riscv_vfmv_v_f_f64m1(0, gvl);
+
+            for (size_t k = 0; k < p; k += gvl){
+                gvl = __riscv_vsetvl_e64m1(p - k);
+                 
+                // Matrix A row
+                vfloat64m1_t va  = __riscv_vle64_v_f64m1(&a[i*p+k], gvl); 
+                // Matrix B column
+                vfloat64m1_t vb = __riscv_vlse64_v_f64m1(&b[k*m+j], m * sizeof(data_t), gvl);
+                
+                // A[0]*B[0], A[1]*B[1],... A[n]*B[n]
+                vprod  = __riscv_vfmacc_vv_f64m1(vprod,va, vb, gvl); 
+  
+            }//k
+            vsum   = __riscv_vfredosum_vs_f64m1_f64m1(vprod,vsum, gvl);
+            c[i*m+j] = __riscv_vfmv_f_s_f64m1_f64(vsum);
+        }//j
+    }//i
+}
+
+
+#else // !USE_RISCV_VECTOR
+
+static void matmul_serial(data_t *a, data_t *b, data_t *c, int n, int m, int p) {
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < m; ++j) {
+      c[i * m + j] = 0;
+      for (int k = 0; k < p; ++k) {
+        c[i * m + j] += a[i * p + k] * b[k * m + j];
+      }
+    }
+}
+
+#endif
+
+
+static bool compare( size_t dm, size_t dn, data_t *a ,data_t *b) {
+    bool result = false;
+    for (int i = 0; i < dm; i++) {
+        for (int j = 0; j < dn; j++) {
+            if(a[i*dn+j] != b[i*dn+j]) {
+              result = true;
+            }
+        }
+ 
+    }
+    return result;
+}
