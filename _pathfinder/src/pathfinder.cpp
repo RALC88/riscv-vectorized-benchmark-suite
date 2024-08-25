f4187c07e28708baa8b4e17b08561804b1343beb
neiel <neiel.leyva@bsc.es>
2024-08-25 18:25:09 +0200

    serial version working and clean code

--- _pathfinder/src/pathfinder.cpp
+++ _pathfinder/src/pathfinder.cpp
@@ -24,8 +24,7 @@ using namespace std;
 /************************************************************************/
 
 //Enable RESULT_PRINT in order to see the result vector, for instruction count it should be disable
-#define RESULT_PRINT
-//#define INPUT_PRINT
+//#define RESULT_PRINT // tmp removed
 #define MAXNAMESIZE 1024 // max filename length
 #define M_SEED 9
 #define MAX_WEIGHT 10
@@ -36,13 +35,12 @@ int* wall;
 int* result;
 string inputfilename;
 string outfilename;
-//#include "timer.h"
 
 void init(int argc, char** argv);
 void run();
 void run_vector();
 void output_printfile(int *dst, string& filename);
-//void init_data(int *data, string& filename );
+
 /*************************************************************************
 *GET_TIME
 *returns a long int representing the time
@@ -61,45 +59,27 @@ float elapsed_time(long long start_time, long long end_time) {
 void init(int argc, char** argv)
 {
     if(argc!=4){
-        printf("Usage: pathfiner width num_of_steps input_file output_file\n");
+        printf("Usage: <cols> <rows> <output_file> \n");
         exit(0);
     }
 
     cols = atoi(argv[1]);
     rows = atoi(argv[2]);
-    //inputfilename = argv[3];
     outfilename = argv[3];
-    //}else{
-    //            printf("Usage: pathfiner width num_of_steps input_file output_file\n");
-    //            exit(0);
-    //    }
     printf("cols: %d rows: %d \n", cols , rows);
 
     wall = new int[rows * cols];
     result = new int[cols];
     
-    //int seed = M_SEED;
-    //srand(seed);
-    /*
-    init_data(wall, inputfilename );
-    */
-    
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
             wall[i*cols+j] = rand() % 10;
         }
     }
-    
-    //for (int j = 0; j < cols; j++)
-    //    result[j] = wall[j];
 
 #ifdef INPUT_PRINT
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
             printf("%d ",wall[i*cols+j]) ;
         }
         printf("\n") ;
@@ -120,15 +100,10 @@ void fatal(char *s)
 int main(int argc, char** argv)
 {
 
-// if(argc != 1 ) {
-//         cout << "Usage: " << argv[0] << " <input_file> " << endl;
-//         exit(1);
-//     }   
 long long start_0 = get_time();
 init(argc,argv);
 long long end_0 = get_time();
 printf("TIME TO INIT DATA: %f\n", elapsed_time(start_0, end_0));
-    //unsigned long long cycles;
 
 #ifndef USE_RISCV_VECTOR
     run();
@@ -146,8 +121,6 @@ void run()
     int min;
     int *src,*dst, *temp;
     
-    //src = (int *)malloc(sizeof(int)*cols);
-    
     printf("NUMBER OF RUNS: %d\n",NUM_RUNS);
     long long start = get_time();
 
@@ -162,7 +135,6 @@ void run()
             temp = src;
             src = dst;
             dst = temp;
-            //#pragma omp parallel for private(min)
             for(int n = 0; n < cols; n++){
               min = src[n];
               if (n > 0)
@@ -172,17 +144,15 @@ void run()
               dst[n] = wall[(t+1)*cols + n]+min;
             }
         }   
-        //delete src;
     }
 
     long long end = get_time();
     printf("TIME TO FIND THE SMALLEST PATH: %f\n", elapsed_time(start, end));
 
 #ifdef RESULT_PRINT
-
     output_printfile(dst, outfilename );
-
 #endif  // RESULT_PRINT
+    
     free(dst);
     free(wall);
     free(src);
@@ -239,9 +209,7 @@ void run_vector()
     printf("TIME TO FIND THE SMALLEST PATH: %f\n", elapsed_time(start, end));
 
 #ifdef RESULT_PRINT
-
     output_printfile(dst, outfilename );
-
 #endif // RESULT_PRINT
 
     free(wall);
@@ -249,26 +217,6 @@ void run_vector()
 }
 #endif // USE_RISCV_VECTOR
 
-/*
-void init_data(int *data,  string&  inputfile ) {
-
-    ifstream fp (inputfile.c_str());
-    assert(fp.is_open()); // were there any errors on opening?
-
-    int value;
-    int i=0;
-    while (!fp.eof()){
-        fp >> data[i];
-        i++;
-    }
-
-    if( !(i-1 == cols*rows) ) {
-        printf("error: the number of elements does not match the parameters i=%d , colsxrows = %d \n",i,cols*rows);
-        exit(1);
-    }
-    fp.close();
-}
-*/
 void output_printfile(int *dst,  string& outfile ) {
     ofstream myfile;
     myfile.open(outfile);
