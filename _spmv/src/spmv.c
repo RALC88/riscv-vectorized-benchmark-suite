60eb7954e116ee5e4593ef9232293ac5a2b77cd3
neiel <neiel.leyva@bsc.es>
2024-10-26 00:05:32 +0200

    vlmax used in vector reduction

--- _spmv/src/spmv.c
+++ _spmv/src/spmv.c
@@ -18,16 +18,14 @@
 
 #ifdef USE_RISCV_VECTOR
 void spmv_intrinsics(const size_t nrows, double *a, uint64_t *ia, uint64_t *ja, double *x, double *y) {
-    size_t   gvl    = __riscv_vsetvlmax_e64m1();
-    _MMR_u64 vthree = __riscv_vmv_v_x_u64m1(3, gvl);
+    size_t   gvl      = __riscv_vsetvlmax_e64m1();
+    _MMR_u64 vthree   = __riscv_vmv_v_x_u64m1(3, gvl);
+    _MMR_f64 vprod    = _MM_SET_f64(0, gvl); 
+    _MMR_f64 part_res = _MM_SET_f64(0, gvl); 
     
     for (size_t row = 0; row < nrows; row++) {
         uint64_t nnz_row = ia[row + 1] - ia[row];
         uint64_t idx     = ia[row];
-        
-                 gvl      = __riscv_vsetvlmax_e64m1();
-        _MMR_f64 vprod    = _MM_SET_f64(0, gvl); 
-        _MMR_f64 part_res = _MM_SET_f64(0, gvl);
 
         for(size_t colid = 0; colid < nnz_row; colid += gvl ) {
                      gvl       = __riscv_vsetvl_e64m1(nnz_row - colid);
@@ -37,9 +35,12 @@ void spmv_intrinsics(const size_t nrows, double *a, uint64_t *ia, uint64_t *ja, 
             _MMR_f64 vx        = __riscv_vluxei64_v_f64m1(x, v_idx_row, gvl);
                      vprod     = _MM_MACC_f64(vprod,va, vx, gvl);
         }
-
+        
+        gvl       = __riscv_vsetvlmax_e64m1();
         part_res  = _MM_REDSUM_f64(vprod, part_res, gvl);
         y[row]    = _MM_VGETFIRST_f64(part_res);
+        vprod     = _MM_SET_f64(0, gvl); 
+        part_res  = _MM_SET_f64(0, gvl);
     }
 }
 #else
