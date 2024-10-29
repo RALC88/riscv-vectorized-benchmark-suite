5d375593bf9ee16a698aa2d479ac9c30124e4990
neiel <neiel.leyva@bsc.es>
2024-10-29 19:25:56 +0100

    verif file added

--- _spmv/src/main.c
+++ _spmv/src/main.c
@@ -10,7 +10,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <stdint.h>
 #include "../../common/riscv_util.h"
+#define TOLERANCE 1e-6
 
 void spmv_intrinsics(const size_t nrows, const size_t ncols, double *a, uint64_t *ia, uint64_t *ja, double *x, double *y); 
 void spmv_serial(const size_t nrows, double *a, uint64_t *ia, uint64_t *ja, double *x, double *y); 
@@ -22,12 +26,14 @@ int main(int argc, char *argv[]){
     uint64_t row, col;
     double value;
     long long start, end;
-
+    bool verification = false;
+    
     if (argc < 2) {
-        printf("Usage: <file.mtx> \n");
+        printf("Usage: <file.mtx> <file.verif> \n *.verif is optional \n");
         return 1;
     }
     
+    if (argv[2] != NULL) verification = true;
 
     if ((f = fopen(argv[1], "r")) == NULL) {
         printf("ERROR: Unable to open file `%s'.\n",argv[1]);
@@ -65,6 +71,25 @@ int main(int argc, char *argv[]){
     ia[M]=NZ;
     
     fclose(f);
+        
+    double *verif = (double *) malloc(M * sizeof(double));
+
+    if(verification == true){
+        if ((f = fopen(argv[2], "r")) == NULL) {
+            printf("ERROR: Unable to open file `%s'.\n",argv[2]);
+            return 1;
+        }
+
+        for (size_t i = 0; i < M; i++) {
+            if (fscanf(f, "%lf,", &verif[i]) != 1) {
+                printf("Error reading file at line %zu\n", i);
+                free(verif);
+                fclose(f);
+                return 1;
+            }
+        }
+        fclose(f);
+    }
 
     for (size_t i = 0; i < N; i++) x[i] = 1.0 ;
 
@@ -80,11 +105,23 @@ int main(int argc, char *argv[]){
     printf("spmv_serial time: %f\n", elapsed_time(start, end));
 #endif
 
+    if(verification == true){
+        for(size_t i=0; i < M ; i++){
+            if (fabs(y[i] - verif[i]) > TOLERANCE) {
+                printf("Verification fail \n");
+                printf("%.17lf  -  %.17lf \n",y[i],verif[i]);
+                return i+1;
+            }
+        }
+        printf("Verification pass \n");
+    }
+
     free(ia);
     free(ja);
     free(a);
     free(x);
     free(y);
+    free(verif);
 
     printf ("done\n");
 
